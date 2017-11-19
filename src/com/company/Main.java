c90e39a30463e9bbce7abe91a2995dd9126d2b6f
catp0wer <catp0wer@mail.ru>
2017-11-19 13:02:22 +0200

    parsing and comparation optimization by dividing the array into length to 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,25 +5,25 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        String word = "CWVIC";
+        String word = "CIVIC";
         System.out.println("The word is " + word);
         char[] arrayFromWord = word.toCharArray();
         System.out.println("The array from String is: " + Arrays.toString(arrayFromWord));
 
-        boolean isPalindrom = true;
-        for (int i = 0; i < arrayFromWord.length; i++) {
+        boolean isPalindrome = true;
+        for (int i = 0; i < arrayFromWord.length/2; i++) {
             if (arrayFromWord[i] == arrayFromWord[arrayFromWord.length - i - 1]) {
                 System.out.println("Compare " + arrayFromWord[i] + " with " + arrayFromWord[arrayFromWord.length - i - 1]);
             }
 
             else{
-                isPalindrom=false;
+                isPalindrome=false;
                 System.out.println("The element " +arrayFromWord[arrayFromWord.length-i-1]+" is not equal to " +arrayFromWord[i] );
                break;
 
             }
         }
-        if(isPalindrom==true){
+        if(isPalindrome==true){
             System.out.println("This is a palindrome");
         }else{
             System.out.println("This is not a palindrome");
