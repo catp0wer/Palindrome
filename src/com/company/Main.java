4d39525f92e04bbccc21fcd13f8552e042cbe434
catp0wer <catp0wer@mail.ru>
2017-11-19 12:39:25 +0200

    adding else statement for negative scenario including a boolean flag

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,22 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        String word = "CIVIC";
+        String word = "CWVIC";
         System.out.println("The word is " + word);
         char[] arrayFromWord = word.toCharArray();
         System.out.println("The array from String is: " + Arrays.toString(arrayFromWord));
 
+        boolean isPalindrom = true;
         for (int i = 0; i < arrayFromWord.length; i++) {
             if (arrayFromWord[i] == arrayFromWord[arrayFromWord.length - i - 1]) {
                 System.out.println("Compare " + arrayFromWord[i] + " with " + arrayFromWord[arrayFromWord.length - i - 1]);
+            }
+
+            else{
+                isPalindrom=false;
+                System.out.println("Elementul " +arrayFromWord[arrayFromWord.length-i-1]+" nu este egal cu elementul " +arrayFromWord[i] );
+               break;
+
             }
         }
     }
