113e52e6ed7309a5b1b2912963d4aad5c624c0a5
catp0wer <catp0wer@mail.ru>
2017-11-19 12:30:57 +0200

    pasing the array and comparing the letters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,15 @@ public class Main {
     public static void main(String[] args) {
 
         String word = "CIVIC";
-        System.out.println("The word is "+word);
+        System.out.println("The word is " + word);
         char[] arrayFromWord = word.toCharArray();
         System.out.println("The array from String is: " + Arrays.toString(arrayFromWord));
 
-
+        for (int i = 0; i < arrayFromWord.length; i++) {
+            if (arrayFromWord[i] == arrayFromWord[arrayFromWord.length - i - 1]) {
+                System.out.println("Compare " + arrayFromWord[i] + " with " + arrayFromWord[arrayFromWord.length - i - 1]);
+            }
         }
     }
+}
 
