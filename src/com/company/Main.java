1c1db31623d2e8a6865c78ade579b9fd39068f95
catp0wer <catp0wer@mail.ru>
2017-11-19 12:14:45 +0200

    converting my string word into an array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         String word = "CIVIC";
+        System.out.println("The word is "+word);
+        char[] arrayFromWord = word.toCharArray();
+        System.out.println("The array from String is: " + Arrays.toString(arrayFromWord));
 
+
+        }
     }
-}
+
